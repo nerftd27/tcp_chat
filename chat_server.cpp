2000b2169f5424522e85ed9d09e462410d37e6dd
nerftd27 <rm27@yandex.ru>
2021-10-22 14:18:17 +0300

    aaaaaaa adadadad

--- chat_server.cpp
+++ chat_server.cpp
@@ -2,18 +2,82 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <iostream>
+#include <cstdlib>
+#include <unistd.h> //close socket
+#include <vector>
+#define BUF_SIZE 1024
+
+int getListenSocket() {
+	int sock;
+	int result;
+	sockaddr_in local;
+	
+	sock= socket(AF_INET, SOCK_STREAM, 0);
+	if (sock<0) {
+		std::cout<<"server.socket() error";
+		std::exit(1);
+	}
+	
+	local.sin_family = AF_INET;
+	local.sin_port = htons(7500);
+	local.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	
+	result = bind( sock, (sockaddr*) &local, sizeof(local) );
+	if (result<0) {
+		std::cout<<"server.bind() error";
+		std::exit(1);
+	}
+		
+	result = listen(sock,5);
+	if (result) {
+		std::cout<<"server.listen() error";
+		std::exit(1);
+	}
+	
+	return sock;
+}
+
+
+void ProcessingMsg(int& sock) {
+	std::string ans="!!!";
+	std::string req;
+	char temp[BUF_SIZE];
+
+	std::cout<<"start ProcessingMsg()\n";
+        //char ans[BUF_SIZE];
+	//char req[BUF_SIZE];
+	recv(sock,temp,BUF_SIZE, 0);
+	std::cout<<"Recieved: "<<req<<std::endl;
+	std::vector<char> buf(ans.begin(), ans.end());
+	buf.push_back('\0');
+
+	send(sock,&buf[0],BUF_SIZE, 0);
+
+	std::cout<<"exit from ProcessingMsg()\n";
+}
 
 int main() {
-	struct sockadrr_in local;
-	int s;
-	int s1;
-	int rc;
-	char buf;
+	int listener;
+	int sock;
+	int res;
+	char buf[BUF_SIZE];
+	int bytes_read=0;
+	std::string req="";
+	std::string ans="";
 
+	listener=getListenSocket();
+	
+	while(1) {
+        	sock = accept(listener, NULL, NULL);
+	        if(sock < 0)
+        	{
+			std::cout<<"server.accept() error";
+            		exit(3);
+        	}
 
-	
-	
-	
-	
+        	ProcessingMsg(sock);
+		close(sock);
+    	}
 	return 0;
 }
