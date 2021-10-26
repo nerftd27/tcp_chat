82f54dc3909169474d6e51f9376646c317ad9103
nerftd27 <rm27@yandex.ru>
2021-10-26 15:35:30 +0300

    added client requests multiply

--- chat_client.cpp
+++ chat_client.cpp
@@ -7,68 +7,118 @@
 #include <unistd.h>	//close(socket)
 
 #include "json.hpp"
+#define BUF_SIZE 20000
 
-int getConnectedSocket() {
+using json = nlohmann::json;
+
+class chat_client {
+	int unsigned id_msg;
 	int sock;
-		
+	char req[BUF_SIZE];
+	char ans[BUF_SIZE];
+public:
+	chat_client();
+	~chat_client();
+	void sendRequest();
+	void recieveAnswer();
+	void assemblyRequest(int command=0);
+};
+
+void chat_client::assemblyRequest(int command) {
+	std::cout<<"command:"<<command<<std::endl;
+	std::string s;
+	json j;
+	if (0==command) {
+		j["id"]=id_msg;
+		j["command"]="HELLO";
+	}
+	if (1==command) {
+		j["id"]=id_msg;
+		j["command"]="login";
+		j["login"]="1";
+		j["password"]="2";
+	}
+	if (2==command) {
+		j["id"]=id_msg;
+		j["command"]="message";
+		j["body"]="test_msg";
+		j["session"]=999;
+	}
+	if (3==command) {
+		j["id"]=id_msg;
+		j["command"]="ping";
+		j["session"]=999;
+	}
+	if (4==command) {
+		j["id"]=id_msg;
+		j["command"]="logout";
+		j["session"]=999;
+	}
+
+	s=j.dump();
+	std::copy(s.begin(),s.end(),req);
+	req[s.size()]='\0';
+}
+
+
+
+
+chat_client::chat_client() {
+	id_msg=1;
 	struct sockaddr_in peer;
 	sock = socket( AF_INET, SOCK_STREAM, 0 );
 	if (sock<0) {
-		std::cout<<"cl.socket() error";
-		return -1;
+		std::cout<<"client.socket() error\n";
+		std::exit(-1);
 	}
 
 	peer.sin_family = AF_INET;
 	peer.sin_port = htons( 7500 );
 	peer.sin_addr.s_addr = inet_addr( "127.0.0.1" );
 
-	int rc = connect(sock,(struct sockaddr*) &peer,sizeof(peer));
-	if ( rc ) {
-		std::cout<<"cl.connect() error";
-		return -2;
+	int res = connect(sock,(struct sockaddr*) &peer,sizeof(peer));
+	if ( res ) {
+		std::cout<<"client.connect() error\n";
+		std::exit(-2);
 	}
-
-	return sock;
 	
 }
 
-int main() {
-	int sock;
-	int rc;
-	char buf[1024];
-	//std::string msg;
-	//std::cout<<"Input msg:\n";
-	//std::cin>>msg;
-	
-	char msg[] = R"(
-    	{
-            "field1":"111",
-            "field2":"222"
-        }
-	)";
+chat_client::~chat_client() {
+	if (sock) close(sock);
+}
 
-	
 
-	sock=getConnectedSocket();
-	
-	if (sock<0) {
-		std::cout<<"cl.Cant get connected socket";       
-		return 1;
+void chat_client::sendRequest() {
+	int res = send(sock,req,BUF_SIZE,0);
+	if (res<=0) {
+		std::cout<<"cl.send() error\n";
 	}
-	
-	
-	rc = send(sock,msg,sizeof(msg),0);
-	if (rc<=0) {
-		std::cout<<"cl.send() error";
-		return 1;
-	}
+	std::cout<<"request !"<<req<<"! sended\n";	
 
-	rc = recv(sock, buf, sizeof(buf), 0 );
-	if ( rc <= 0 )
-		std::cout<<"cl.recv() error";
+	id_msg++;
+}
+
+void chat_client::recieveAnswer() {
+	int res = recv(sock, ans, BUF_SIZE, 0 );
+	if ( res <= 0 )
+		std::cout<<"cl.recv() error\n";
 	else
-		std::cout<<"from srv: "<<buf;
-	close(sock);
+		std::cout<<"answer !"<<ans<<"!  recieved\n ";
+}
 
+
+int main() {
+	chat_client client1;
+	for(int command=0;;) {
+		std::cout<<"input command:";
+		std::cin>>command;
+		if (-1==command) break;
+		std::cout<<std::endl;
+
+		client1.assemblyRequest(command);
+		client1.sendRequest();
+		client1.recieveAnswer();
+	}
 	return 0;;
 }
--- chat_server.cpp
+++ chat_server.cpp
@@ -11,13 +11,51 @@ using json = nlohmann::json;
 
 #define BUF_SIZE 1024
 
-int getListenSocket() {
-	int sock;
+struct chat_user {
+	char* login;
+	char* password;
+};
+
+
+struct chat_session {
+	long UUID;
+	chat_user user;
+	int client_socket;
+};
+	
+class chat_server {
+	int listener;
+	std::vector<int> accepted_sockets;
+	std::vector<chat_session> sessions;
+	char req[BUF_SIZE];
+	char ans[BUF_SIZE];
+
+
+	void getListenSocket();
+public:
+	chat_server();
+	~chat_server();
+	void Processing();
+};
+
+chat_server::chat_server() {
+	getListenSocket();
+}
+
+chat_server::~chat_server() {
+	for(auto i:accepted_sockets) {
+		close(i);
+	}
+	if (listener) close(listener);
+}
+
+
+void chat_server::getListenSocket() {
 	int result;
 	sockaddr_in local;
 	
-	sock= socket(AF_INET, SOCK_STREAM, 0);
-	if (sock<0) {
+	listener= socket(AF_INET, SOCK_STREAM, 0);
+	if (listener<0) {
 		std::cout<<"server.socket() error";
 		std::exit(1);
 	}
@@ -27,23 +65,54 @@ int getListenSocket() {
 	local.sin_addr.s_addr = htonl(INADDR_ANY);
 	
 	
-	result = bind( sock, (sockaddr*) &local, sizeof(local) );
+	result = bind( listener, (sockaddr*) &local, sizeof(local) );
 	if (result<0) {
 		std::cout<<"server.bind() error";
 		std::exit(1);
 	}
 		
-	result = listen(sock,5);
+	result = listen(listener,5);
 	if (result) {
 		std::cout<<"server.listen() error";
 		std::exit(1);
 	}
-	
-	return sock;
+
 }
 
 
-std::string ProcessingJson(int& sock) {
+void chat_server::Processing() {
+	//getting new socket
+	int sock = accept(listener, NULL, NULL);
+	if(sock < 0)
+        {
+		std::cout<<"server.accept() error";
+            	exit(3);
+        }
+	accepted_sockets.push_back(sock);
+
+
+	//processing each opened socket
+	for (auto i:accepted_sockets) {
+
+		recv(i,req,BUF_SIZE,0);
+		std::cout<<"Recieved msg :"<<req<<std::endl;
+	
+		*ans='Z';
+		send(i,ans,BUF_SIZE, 0);
+	
+		std::cout<<"Sended msg:"<<ans<<std::endl;
+	}
+
+	
+
+	//free res
+	//close(*accepted_sockets.end());
+	//accepted_sockets.pop_back();
+}
+
+
+
+/*std::string ProcessingJson(int& sock) {
 
 	char temp[BUF_SIZE];
 	recv(sock,temp,BUF_SIZE, 0);
@@ -52,41 +121,17 @@ std::string ProcessingJson(int& sock) {
 	std::string req=j.value("field2","oops");
 	std::cout<<"Recieved: "<<req<<std::endl;
 	return req;
-}
+}*/
 
 
-void ProcessingMsg(int& sock) {
-	std::cout<<"start ProcessingMsg()\n";
-	std::string ans=ProcessingJson(sock);
 
-	std::vector<char> buf(ans.begin(), ans.end());
-	buf.push_back('\0');
-	send(sock,&buf[0],BUF_SIZE, 0);
-
-	std::cout<<"exit from ProcessingMsg()\n";
-}
 
 int main() {
-	int listener;
-	int sock;
-	int res;
-	char buf[BUF_SIZE];
-	int bytes_read=0;
-	std::string req="";
-	std::string ans="";
-
-	listener=getListenSocket();
+	chat_server srv1;
 	
 	while(1) {
-        	sock = accept(listener, NULL, NULL);
-	        if(sock < 0)
-        	{
-			std::cout<<"server.accept() error";
-            		exit(3);
-        	}
-
-        	ProcessingMsg(sock);
-		close(sock);
+        	srv1.Processing();
+	
     	}
 	return 0;
 }
